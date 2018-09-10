f817d8053eb60a4eb1c22dde8dafc2202d87efa9
eagleEggs <29800532+eagleEggs@users.noreply.github.com>
2018-09-09 23:24:39 -0400

    Update world.py

--- world.py
+++ world.py
@@ -11,6 +11,102 @@ class World():
 		self.monsters = monsters
 		self.hiddenItems = hiddenItems
 
+class worldGen(World):
+	def __init__(self, worldType, *args):
+		super(worldGen, self).__init__(*args)
+		self.worldType = worldType
+
+		level = []									# set up level array
+
+		empties = 0
+		bosses = 0
+		chests = 0
+		monsters = 0
+		hItems = 0
+
+		#bossPlaced = 0
+		if self.worldType == "auto":
+			iterate = int(self.levels)
+			while iterate > 0:
+				for lev in range(0, iterate):					# replace array with random entities
+					pick = random.randint(1, 6)
+					if pick == 1:
+						level.append("E")
+						empties = empties + 1
+						iterate = iterate - 1
+					#if pick == 2 and bossPlaced == 0:           # originally set for one boss per level
+					if pick == 2:
+						level.append("B")
+						#bossPlaced = 1
+						bosses = bosses + 1
+						iterate = iterate - 1
+					if pick == 3:
+						level.append("C")
+						chests = chests + 1
+						iterate = iterate - 1
+					if pick == 4:
+						level.append("M")
+						monsters = monsters + 1
+						iterate = iterate - 1
+					if pick == 5:
+						level.append("H")
+						hItems = hItems + 1
+						iterate = iterate - 1
+
+			print("World Generated: \n{}".format(level))
+			print("")
+			print("Levels: {}, Empties: {}, Bosses: {}, Chests: {}, Monsters: {}, Hidden Items: {}".format(self.levels, empties, bosses, chests, monsters, hItems))
+			print("")
+			print("")
+			regen = input("Regenerate? (y/n)")
+			print("")
+			if regen == "y":
+				worldGen("auto", random.randint(1,99), self.empty, self.bosses, self.chests, self.monsters, self.hiddenItems)
+
+		if self.worldType == "manual":
+			
+			empties = int(self.empty)
+			bosses = int(self.bosses)
+			chests = int(self.chests)
+			monsters = int(self.monsters)
+			hItems = int(self.hiddenItems)
+
+			iterate = int(self.levels)
+			while iterate > 0:
+				for lev in range(0, iterate):
+					pick = random.randint(1, 6)
+					if pick == 1 and empties != 0:
+						level.append("E")
+						empties = empties - 1
+						iterate = iterate - 1
+					if pick == 2 and bosses != 0:
+						level.append("B")
+						bosses = bosses - 1
+						iterate = iterate - 1
+					if pick == 3 and chests != 0:
+						level.append("C")
+						chests = chests - 1
+						iterate = iterate - 1
+					if pick == 4 and monsters != 0:
+						level.append("M")
+						monsters = monsters - 1
+						iterate = iterate - 1
+					if pick == 5 and hItems != 0:
+						level.append("H")
+						hItems = hItems - 1
+						iterate = iterate - 1
+
+			print("World Generated: \n{}".format(level))
+			print("")
+			print("Levels: {}, Empties: {}, Bosses: {}, Chests: {}, Monsters: {}, Hidden Items: {}".format(self.levels, empties, bosses, chests, monsters, hItems))
+			print("")
+			print("")
+			regen = input("Redo? (y/n)")
+			print("")
+			if regen == "y":
+				worldGen("manual", self.levels, self.empty, self.bosses, self.chests, self.monsters, self.hiddenItems)
+
+
 		@property
 		def levels(self):
 			return self.__levels
@@ -22,137 +118,13 @@ class World():
 		return "Levels: {}".format(self.levels)
 
 
-class Map(World):
+class WorldStep(World):
+	def __init__(self, currentLevel, nextLevel):
+		self.currentLevel = currentLevel
+		self.nextLevel = nextLevel
 
-
-	def __init_(self, home, castle, emptyHorizontal, filledHorizontal):
-		self.char = self.char
-		self.home = self.mapHouse
-		self.casle = self.mapCastle
-		self.emptyHorizontal = self.mapEmptyHorizontal
-		self.filledHorizontal = self.mapFilledHorizontal
-
-	def mapHome():
-
-
-		print("""\
-
-  /\ 
- /  \ 
-/    \ 
-| __ | 
-|_||_| 
- home
-	""")
+	def progress(self):
+		self.currentLevel = self.nextLevel
 
 
 
-	def mapCastle():
-
-		print("""
-		___  ___  ___ 
-		| |__| |__| | 
-		|           | 
-		| O  ___  O | 
-		|  /     \  | 
-		|__|_____|__| 
-		   castle 
-				""")
-
-	def mapEmptyHorizontal():
-
-		print("""
-    _
-    _""")
-
-
-
-	def mapFilledHorizontal(key):
-
-		mapFilledHorizontal = """
-	_
-	""""{}".format(key)
-		print (mapFilledHorizontal)
-
-
-
-
-	# | |__________             # ______________| |
-	# |	 ___B______C|			# _____C____M_____|
-	# |M|						#
-								#
-
-
-
-
-
-
-
-class Biomes():
-
-	biomeList = {
-		'forest':{
-			'rain': 1
-		},
-		'desert':{
-			'rain': 0
-		},
-		'arctic':{
-			'rain': 0
-		}
-
-	}
-
-	def __init__(self, biome):
-		self.biome = self.biomeList[biome]
-
-
-def GenerateWorld(generatedWorld):
-
-	level = []									# set up level array
-
-	generated = generatedWorld
-
-	empties = 0
-	bosses = 0
-	chests = 0
-	monsters = 0
-	hItems = 0
-
-	#bossPlaced = 0
-	iterate = int(generated.levels)
-	while iterate > 0:
-		for lev in range(0, iterate):					# replace array with random entities
-			pick = random.randint(1, 6)
-			if pick == 1:
-				level.append("E")
-				empties = empties + 1
-				iterate = iterate - 1
-			#if pick == 2 and bossPlaced == 0:           # originally set for one boss per level
-			if pick == 2:
-				level.append("B")
-				#bossPlaced = 1
-				bosses = bosses + 1
-				iterate = iterate - 1
-			if pick == 3:
-				level.append("C")
-				chests = chests + 1
-				iterate = iterate - 1
-			if pick == 4:
-				level.append("M")
-				monsters = monsters + 1
-				iterate = iterate - 1
-			if pick == 5:
-				level.append("H")
-				hItems = hItems + 1
-				iterate = iterate - 1
-
-	print("World Generated: \n{}".format(level))
-	print("")
-	print("Levels: {}, Empties: {}, Bosses: {}, Chests: {}, Monsters: {}, Hidden Items: {}".format(generated.levels, empties, bosses, chests, monsters, hItems))
-	print("")
-	print("")
-	regen = input("Regenerate? (y/n)")
-	print("")
-	if regen == "y":
-		game.Setup()
