c4df9e2cdf53fc6da89f6e214c93e8d2cca40206
eagleEggs <29800532+eagleEggs@users.noreply.github.com>
2018-09-09 23:24:17 -0400

    Update game.py

--- game.py
+++ game.py
@@ -4,44 +4,7 @@ import characters
 import world
 import logo
 import inventory
-import cards
 import battle
-
-
-
-def Adventure(player):
-
-	while True:
-		print("What are you going to do?")
-		print("")
-		print("[ l:Look Around | m:Move Forward \n"
-			"[ i:Inventory | p:Player Info")
-		print("")
-		answer = input(">>> ")
-		if answer == "l":
-			print("")
-			print("You look around...")
-			LookAround()
-		if answer == "p":
-			print("")
-			print(player)
-		if answer == "m":
-			print("You decided to move forward")
-			#moveForward()
-
-def RandomRoll():
-	rand = random.randint(1,101)
-	random = random.randint(1, 101)
-	if rand == random:
-		return 
-
-
-def LookAround():
-	print("Where would you like to look?")
-	print("")
-	print("[ g:Ground | s:Sky | e:Elsewhere")
-	print("")
-	answer = input(">>>")
 
 
 def introStory(player):
@@ -64,11 +27,12 @@ def Setup():
 	if classChoice == "w":
 		player = characters.Warrior("plunger", "{}".format(inputName), 100, 100, 0, 100, 1)
 	print("")
+
 	worldType = input("Auto Generate [a], or Manual World Generation [m]?")
 	print("")
 	if worldType == "a":
-		generatedWorld = world.World(random.randint(1,99),random.randint(1,99),random.randint(1,99),random.randint(1,99),random.randint(1,99),random.randint(1,99))
-		world.GenerateWorld(generatedWorld)
+		generatedWorld = world.worldGen("auto", random.randint(1,99),random.randint(1,99),random.randint(1,99),random.randint(1,99),random.randint(1,99),random.randint(1,99))
+		print("")
 	if worldType == "m":
 		nLevels = input("Enter number of Levels: ")
 		nEmpty = input("Enter number of Empty Levels :")
@@ -77,8 +41,7 @@ def Setup():
 		nMonsters = input("Enter number of Monsters: ")
 		nHiddenItems = input("Enter number of Hidden Items: ")
 		
-		generatedWorld = world.World(nLevels, nEmpty, nBosses, nChests, nMonsters, nHiddenItems)
-		world.GenerateWorld(generatedWorld)
+		generatedWorld = world.worldGen("manual", nLevels, nEmpty, nBosses, nChests, nMonsters, nHiddenItems)
 		print("")
 
 	introStory(player)
@@ -91,65 +54,3 @@ def Setup():
 if __name__ == '__main__':
 
 	Setup()
-
-
-
-
-
-	#########
-	###POC###
-	#########
-
-	# loop
-
-	#inputName = input("Enter Your Name Adventurer: ")
-	#player = Warrior("plunger", "{}".format(inputName), 100, 100, 0, 100)
-
-	#print("You begin your adventure {}, upon waking amidst the showing rain of the darkest forest...".format(inputName))
-	#input("Press Enter to Get up from your long sleep...")
-
-	#print("You hear something coming closer...")
-	#battleSuspense()
-	#enemy = Vampire(1,1,"Vampire",200,99, 0, 400)
-	#print ("It's a {}!!!".format(enemy.name))
-	#print("What are you going to do?")
-	#print("[ e:Attack | s:Retreat \n"
-	#"[ i:Inventory | p:Player Info")
-	#answer = input(">>> ")
-	#if answer == "e":
-	#	print("You Attacked {}".format(enemy.name))
-	#	player.attacks(enemy)
-	#if answer == "p":
-	#	print(player)
-
-
-
-
-
-###############
-### Testing ###
-###############
-
-#vampireEnemy = Vampire(1,1,"Alucard",200,99, 0, 400)
-#vampireEnemy.fly()
-
-
-
-
-#print(vampireEnemy.health)
-#vampireEnemy.attacks(vampireEnemy)
-#print(vampireEnemy.health)
-#blobEnemy = Blob(0,0,100,10,0,100, "Blobbly")
-#blobEnemy.duplicate(4)
-#blobDupe = blobEnemy
-#print(blobEnemy.health)
-#print(blobDupe.health)
-#player = Warrior("stick", "StrongMan", 100,0,200, 99)
-#print(player.name)
-#print(player.health)
-#player.attacks(player)
-#print(player.health)
-#world = World("forest")
-#print(world)
-
-
